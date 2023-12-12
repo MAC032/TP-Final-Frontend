a239c36fafd7cbc4fdb48fa606c2fd6b3306c300
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-12-12 13:41:38 -0300

    Evento cargarImagen en producto_editar

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -1,60 +1,69 @@
-console.log(location.search) // lee los argumentos pasados a este formulario
-var id=location.search.substr(4)
-console.log(id)
-const { createApp } = Vue
+console.log(location.search); // lee los argumentos pasados a este formulario
+let id = location.search.substr(4);
+console.log(id);
+
+const { createApp } = Vue;
 createApp({
-data() {
-return {
-id:0,
-nombre:"",
-imagen:"",
-stock:0,
-precio:0,
-url:'https://handana.pythonanywhere.com/productos/'+id,
-}
-},
-methods: {
-fetchData(url) {
-fetch(url)
-.then(response => response.json())
-.then(data => {
-console.log(data)
-this.id=data.id
-this.nombre = data.nombre;
-this.imagen=data.imagen
-this.stock=data.stock
-this.precio=data.precio
-})
-.catch(err => {
-console.error(err);
-this.error=true
-})
-},
-modificar() {
-let producto = {
-nombre:this.nombre,
-precio: this.precio,
-stock: this.stock,
-imagen:this.imagen
-}
-var options = {
-body: JSON.stringify(producto),
-method: 'PUT',
-headers: { 'Content-Type': 'application/json' },
-redirect: 'follow'
-}
-fetch(this.url, options)
-.then(function () {
-alert("Registro modificado")
-window.location.href = "./productos.html";
-})
-.catch(err => {
-console.error(err);
-alert("Error al Modificar")
-})
-}
-},
-created() {
-this.fetchData(this.url)
-},
-}).mount('#app')
+  data() {
+		return {
+			id: 0,
+			nombre: "",
+			imagen: "",
+			stock: 0,
+			precio: 0,
+			url:'https://handana.pythonanywhere.com/productos/' + id,
+		};
+	},
+	methods: {
+		fetchData(url) {
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					console.log(data);
+					this.id = data.id;
+					this.nombre = data.nombre;
+					this.imagen = data.imagen;
+					this.stock = data.stock;
+					this.precio = data.precio;
+				})
+				.catch(err => {
+					console.error(err);
+					this.error = true;
+				});
+		},
+		modificar() {
+			let producto = {
+				nombre: this.nombre,
+				precio: this.precio,
+				stock: this.stock,
+				imagen: this.imagen
+			};
+			let options = {
+				body: JSON.stringify(producto),
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				redirect: 'follow',
+			};
+			fetch(this.url, options)
+				.then(() => {
+					alert("Registro modificado");
+					window.location.href = "./productos.html";
+				})
+				.catch(err => {
+					console.error(err);
+					alert("Error al Modificar");
+				});
+		},
+		cargarImagen(event) {
+			const file = event.target.files[0];
+
+			if (file) {
+					// Convierte la imagen a un formato de URL legible por el navegador
+					this.imagen = URL.createObjectURL(file);
+			}
+		}
+	},
+	created() {
+		this.fetchData(this.url);
+	},
+}).mount('#app');
