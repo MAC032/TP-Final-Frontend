cc82fd86ee398d9412084115b058b31ec4659611
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-12-14 14:08:25 -0300

    link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function prevButton() { window.location.href = '../index.html'; }
 function nextButton() { window.location.href = '../index.html'; }
 function homeButton() { window.location.href = './index.html'; }
-function productosButton() { window.location.href="productos.html"; }
+function productosButton() { <a class="dropdown-item" href="productos.html">Produtos</a>; }
 function clasicosButton() { window.location.href = './pages/clasicos.html'; }
 function aventuraButton() { window.location.href = './pages/aventura.html'; }
 function subirButton() { window.scrollTo({top: 0}) }
