382219f1127718a2a731e113fffcfc93d993d23d
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-12-14 13:30:51 -0300

    index

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function prevButton() { window.location.href = '../index.html'; }
+function nextButton() { window.location.href = '../index.html'; }
+function homeButton() { window.location.href = './index.html'; }
+function accionButton() { window.location.href = './pages/accion.html'; }
+function clasicosButton() { window.location.href = './pages/clasicos.html'; }
+function aventuraButton() { window.location.href = './pages/aventura.html'; }
+function subirButton() { window.scrollTo({top: 0}) }
+
+function agregarElementos() {
+    let pContainer = document.getElementById('container');
+    juegos.forEach(function(juego) {
+    let pDiv = document.createElement('div');
+    pDiv.classList.add('grid-item');
+    pDiv.innerHTML = juego.foto;
+    pContainer.appendChild(pDiv);
+  });
+}
+
+function domLoaded() {document.addEventListener('DOMContentLoaded', agregarElementos())};
+
